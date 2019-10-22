80984d01b07e8169101e6885c3b85fc2350a3f84
Miles Cruz <cruz.r.miles@gmail.com>
2019-10-21 20:07:27 -0500

    Update models.py

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class Topic(models.Model):
+#class Topic(models.Model):
+
+class Entry(models.Model):
     '''A topic the user is learning about'''
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
